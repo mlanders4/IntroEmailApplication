5065759ee21f7aa2f26f356b86c2b0dab49a7c49
Jared Hoyt <jared.v.hoyt@gmail.com>
2023-09-28 11:11:38 -0500

    Update whitespace

--- IntroEmailApplication/Program.cs
+++ IntroEmailApplication/Program.cs
@@ -3,53 +3,53 @@ using MimeKit;
 
 namespace IntroEmailApplication
 {
-  internal class Program
-  {
-    private const int SmtpPort = 465;
-    private const bool IsSslConnection = true;
-
-    static void Main(string[] args)
+    internal class Program
     {
-      Console.WriteLine("Welcome to the command line email client!");
+        private const int SmtpPort = 465;
+        private const bool IsSslConnection = true;
 
-      var user_wants_to_send_message = true;
-
-      while (user_wants_to_send_message)
-      {
-        Console.WriteLine();
-        Console.WriteLine("New Message");
-
-        var mail = new MimeMessage();
-
-        mail.From.Add(new MailboxAddress("Jared Hoyt", "jaredhoyt@gmail.com")); // TODO: update to your own name and email address
-
-        Console.Write("To: ");
-        mail.To.Add(new MailboxAddress("", Console.ReadLine()));
-
-        Console.Write("Subject: ");
-        mail.Subject = Console.ReadLine();
-
-        Console.Write("Body: ");
-        mail.Body = new TextPart("plain") { Text = Console.ReadLine() };
-
-        using (var client = new SmtpClient())
+        static void Main(string[] args)
         {
-          client.Connect("smtp.gmail.com", SmtpPort, IsSslConnection);
-          client.Authenticate("jaredhoyt", "abcdefghijklmnop"); // TODO: update to your own username and APP PASSWORD (this is different from your normal password)
-          client.Send(mail);
-          client.Disconnect(true);
+            Console.WriteLine("Welcome to the command line email client!");
 
-          Console.WriteLine("Message sent successfully!");
+            var user_wants_to_send_message = true;
+
+            while (user_wants_to_send_message)
+            {
+                Console.WriteLine();
+                Console.WriteLine("New Message");
+
+                var mail = new MimeMessage();
+
+                mail.From.Add(new MailboxAddress("Jared Hoyt", "jaredhoyt@gmail.com")); // TODO: update to your own name and email address
+
+                Console.Write("To: ");
+                mail.To.Add(new MailboxAddress("", Console.ReadLine()));
+
+                Console.Write("Subject: ");
+                mail.Subject = Console.ReadLine();
+
+                Console.Write("Body: ");
+                mail.Body = new TextPart("plain") { Text = Console.ReadLine() };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", SmtpPort, IsSslConnection);
+                    client.Authenticate("jaredhoyt", "abcd efgh ijkl mnop"); // TODO: update to your own username and APP PASSWORD (this is different from your normal password)
+                    client.Send(mail);
+                    client.Disconnect(true);
+
+                    Console.WriteLine("Message sent successfully!");
+                }
+
+                Console.WriteLine();
+                Console.Write("Would you like to send another email? (Y/N): ");
+
+                if (Console.ReadKey().Key != ConsoleKey.Y)
+                {
+                    user_wants_to_send_message = false;
+                }
+            }
         }
-
-        Console.WriteLine();
-        Console.Write("Would you like to send another email? (Y/N): ");
-
-        if (Console.ReadKey().Key != ConsoleKey.Y)
-        {
-          user_wants_to_send_message = false;
-        }
-      }
     }
-  }
 }
